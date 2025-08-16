189a2d5c09094de01c0fbfbaaa1e31a695bfd984
Oz Blech <ozblech87@gmail.com>
2025-08-16 19:45:11 +0300

    Add git ignore

--- EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e98c6d59204fcef23821d1e0e8e4627a6d3b47b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fa3947b6ac32f1701b09aa481c2b64ea5835d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
